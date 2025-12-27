88fe51b3409a4229caa009349d5d23bdfad24d1d
Sambhav <sambhavduhan007@gmail.com>
2025-12-27 17:06:33 +0530

    Implement basic operator examples in code.py
    Add examples of arithmetic, logical, and comparison operators.

---
+++ Day-4/code.py
@@ -0,0 +1,32 @@
+#Arthmetic Operators, logical operators and comparison operators
+#a=2
+#b=2
+
+'''print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a % b)   # Modulus
+print(a ** b)  # Power
+print(a // b)  # Floor division'''
+
+'''print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)'''
+
+'''x=10 
+y=5 
+
+print(x > 5 and x < 20)
+print(x > 5 or x < 5)
+print(not(x > 5))
+print(not(y>10))'''
+
+age = int(input("Enter your age: "))
+print(age > 18)
+
+
+
