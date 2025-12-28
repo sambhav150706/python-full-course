66592e583741aebbd493018a54617c0f2d78372b
Sambhav <sambhavduhan007@gmail.com>
2025-12-28 19:02:11 +0530

    Add examples of if-else statements in Python
    Added examples of if-else statements in Python, including simple if, if-else, if-elif-else, and checks for even/odd and positive/negative numbers.

---
+++ Day-05/Codes.py
@@ -0,0 +1,51 @@
+# Author: CodeFromZero
+# Topic: Day 05 - if-else Statements
+
+
+'''# Example 1: Simple if statement
+age = 20
+
+if age >= 18:
+    print("You are an adult")
+
+
+# Example 2: if-else statement
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+
+# Example 3: if-elif-else statement
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+
+# Example 4: Even or Odd
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+
+# Example 5: Positive, Negative or Zero
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")'''
