bbde5ea8533151fc98cf50507f45ebaf0c6c08c1
Sambhav <sambhavduhan007@gmail.com>
2025-12-29 19:44:24 +0530

    Create Code.py

---
+++ Day-06/Code.py
@@ -0,0 +1,28 @@
+## 💻 `loops.py`
+
+#python
+# Day 06 - Loops in Python
+
+# for loop example
+'''for i in range(1, 6):
+    print("Python", i)
+    '''
+'''# printing numbers 1 to 10
+for i in range(1, 11):
+    print(i)'''
+
+'''# while loop example
+i = 1
+while i <= 5:
+    print("Hello")
+    i += 1'''
+
+'''# sum of first 5 numbers
+sum = 0
+i = 1
+while i <= 5:
+    sum += i
+    i += 1
+
+print("Sum is:", sum)'''
+
